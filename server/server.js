b7d0d670dda4f93e47c571e20aea0d1f64f2435b
AdriR-R <rodriguezramirezadrianismael6@gmail.com>
2024-12-24 17:59:33 -0600

    config server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import { usersRouter } from './routes/users.js'
+import path from 'path'
+
 const app = express()
+
 app.use(express.json())
+app.set('views', path.join(process.cwd(), 'server', 'views'))
+app.set('view engine', 'ejs')
 app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.render('example', { user: 'Home' })
+})
 
 app.use('/users', usersRouter)
 // app.use('/tasks', tasksRouter)
