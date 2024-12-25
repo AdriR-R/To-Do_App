76b3def3ce8dbde4ef02da8f7cd868143073b086
AdriR-R <rodriguezramirezadrianismael6@gmail.com>
2024-12-24 19:51:55 -0600

    configurar cookies v1.0

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { usersRouter } from './routes/users.js'
+import cookieParser from 'cookie-parser'
 const app = express()
 app.use(express.json())
 app.disable('x-powered-by')
+app.use(cookieParser())
 
 app.use('/users', usersRouter)
 // app.use('/tasks', tasksRouter)
