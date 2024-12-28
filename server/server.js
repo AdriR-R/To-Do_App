533be39ccf39e56627608eddaef3ecc005148f1c
AdriR-R <rodriguezramirezadrianismael6@gmail.com>
2024-12-27 19:08:14 -0600

    implementación de autenticación JWT y creación de vistas de login y sesión protegida

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { usersRouter } from './routes/users.js'
 import path from 'path'
 import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
 const app = express()
 
 app.use(express.json())
@@ -10,8 +11,36 @@ app.set('views', path.join(process.cwd(), 'server', 'views'))
 app.set('view engine', 'ejs')
 app.disable('x-powered-by')
 
+app.use((req, res, next) => {
+  if (req.url === '/') {
+    res.set('Cache-Control', 'no-store')
+    return next()
+  }
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.render('example', { user: 'Home' })
+  const token = req.cookies.access_token
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    return res.render('login', { username: decoded.username })
+  } catch (err) {
+    res.render('login')
+  }
+})
+
+app.get('/protected', (req, res) => {
+  const token = req.cookies.access_token
+  if (!token) {
+    return res.redirect('/')
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    res.render('protected', { username: decoded.username })
+  } catch (err) {
+    console.error(err)
+    return res.redirect('/')
+  }
 })
 
 app.use('/users', usersRouter)
